7fd0ff6a953af4e407f516b7211134da932103e7
Mike Sharov <msharov@users.sourceforge.net>
2013-02-23 15:35:51 -0500

    Directly check for execinfo.h instead of linux and gcc

--- bktrace.cc
+++ bktrace.cc
@@ -6,7 +6,7 @@
 #include "bktrace.h"
 #include "sostream.h"
 #include "mistream.h"
-#if linux && __GNUC__
+#if HAVE_EXECINFO_H
     #include <execinfo.h>
 #else
     static inline int backtrace (void**, int)			{ return (0); }
